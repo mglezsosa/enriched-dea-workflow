01bf1f7920fd727038da300edba41c4fa8b6c456
Miguel González <sosa@uma.es>
2019-01-19 16:59:20 +0100

    Add support for enrichment analysis and kegg pathways visualization.
    Removed -a option.

--- R/3_DEA.R
+++ R/3_DEA.R
@@ -5,13 +5,16 @@
 # Pass arguments to the script
 args = commandArgs(trailingOnly=TRUE)
 
-if (length(args)<6) {
-        stop("Two first arguments must be cancer types ('Basal', 'TNBC' or 'LumA'), the third must be a valid p-value, the fourth must be a valid fold-change value and the fifth and sixth must be valid output relative paths for pdf and csv files.", call.=FALSE)
+if (length(args)<5) {
+        stop("Two first arguments must be cancer types ('Basal', 'TNBC' or 'LumA'), the third must be a valid p-value, the fourth must be a valid fold-change value and the fifth must be a valid output relative path for pdf and csv files.", call.=FALSE)
 }
 
 if (args[1] == args[2]) {
         stop("Cannot perform DEA with the same subsets.")
 }
+
+cancertype1 = args[1]
+cancertype2 = args[2]
 
 # Load the expression data
 setwd("R")
@@ -28,24 +31,25 @@ loadpkg("calibrate") # To label the volcano plot
 
 # Pick the datasets to analyse
 
-if(args[1] == "Basal"){d1 = brca_rnaseq.basal}
-if(args[1] == "TNBC"){d1 = brca_rnaseq.tnbc}
-if(args[1] == "LumA"){d1 = brca_rnaseq.luminal}
-if(args[2] == "Basal"){d2 = brca_rnaseq.basal}
-if(args[2] == "TNBC"){d2 = brca_rnaseq.tnbc}
-if(args[2] == "LumA"){d2 = brca_rnaseq.luminal}
+if(cancertype1 == "Basal"){d1 = brca_rnaseq.basal}
+if(cancertype1 == "TNBC"){d1 = brca_rnaseq.tnbc}
+if(cancertype1 == "LumA"){d1 = brca_rnaseq.luminal}
+if(cancertype2 == "Basal"){d2 = brca_rnaseq.basal}
+if(cancertype2 == "TNBC"){d2 = brca_rnaseq.tnbc}
+if(cancertype2 == "LumA"){d2 = brca_rnaseq.luminal}
 
-cat("Differential Expression analysis of breast cancer subtypes:",args[1], "vs",args[2], "\n")
+cat("Differential Expression analysis of breast cancer subtypes:", cancertype1, "vs", cancertype2, "\n")
 
 
 # We Combine the two matrices for gene differential expression analysis (DEA). Further preprocessing included the removal of expression estimates with counts in less than 20% of cases.
 rnaseq.for.de <- cbind(d1, d2)
 counts = rnaseq.for.de[apply(rnaseq.for.de,1,function(x) sum(x==0))<ncol(rnaseq.for.de)*0.8,]
 
-# Create a design matrix thar contains the RNA samples that are applied to each category (TNBC vs luminal)
-df.l <- data.frame(sample = colnames(d1), status = rep(0, length(colnames(d1))))
-df.t <- data.frame(sample = colnames(d2), status = rep(1, length(colnames(d2))))
-df <- rbind(df.t,df.l)
+# Create a design matrix thar contains the RNA samples that are applied to each category
+df <- rbind(
+        data.frame(sample = colnames(d1), status = rep(1, length(colnames(d1)))),
+        data.frame(sample = colnames(d2), status = rep(0, length(colnames(d2))))
+)
 design <- model.matrix(~ status, data = df)
 
 
@@ -62,7 +66,7 @@ fit <- eBayes(fit)
 lfc = as.double(args[4])
 pval = as.double(args[3])
 
-diff.exp.df <- topTable(fit, coef = "status", n = Inf, sort = "p", p = 0.05) # Positive log-fold-changes mean higher expression in d1
+diff.exp.df <- topTable(fit, coef = "status", n = Inf, sort = "p", p = pval) # Positive log-fold-changes mean higher expression in d1
 diff.exp.df$gene.name <- rownames(diff.exp.df)
 # With the code above, perform the DEA. First, we convert the read counts to log2-cpm, with associated weights, ready for linear modelling. As read counts follow a negative binomial distribution, which has a mathematical theory less tractable than that of the normal distribution, RNAseq data was normalised with the voom methodology. The voom method estimates the mean-variance of the log-counts and generates a precision weight for each observation. This way, a comparative analysis can be performed with all bioinformatic workflows originally developed for microarray analyses ([see this](https://bioconductor.org/packages/3.7/bioc/vignettes/CVE/inst/doc/WGCNA_from_TCGA_RNAseq.html) and this paper Charity W Law et al. voom: Precision weights unlock linear model analysis tools for RNA-seq read counts. In: Genome biology 15.2 (Jan. 2014), R29–R29).
 
@@ -70,13 +74,16 @@ if (!nrow(diff.exp.df)) {
     stop(paste0("No genes filfill the given restrictions: pvalue=", pval, ", logfoldchange=", lfc, "."))
 }
 
+# Save the differential expression dataframe and types
+save(diff.exp.df, cancertype1, cancertype2, file = "diff.exp.RData")
+
 # Output, Volcano plot
 tab = data.frame(logFC = diff.exp.df$logFC, negLogPval = -log10(diff.exp.df$adj.P.Val))
 tab2 = data.frame(logFC = diff.exp.df$logFC, negLogPval = -log10(diff.exp.df$adj.P.Val), Gene=diff.exp.df$gene.name)
 
-write.csv(filter(tab2, abs(logFC) > lfc & negLogPval > -log10(pval)), paste0("../", args[6])) # write output
+write.csv(filter(tab2, abs(logFC) > lfc & negLogPval > -log10(pval)), paste0("../", args[5], ".csv")) # write output
 
-pdf(file = paste0("../", args[5]), width = 9, height = 4.5)
+pdf(file = paste0("../", args[5], ".pdf"), width = 9, height = 4.5)
 par(mar = c(5, 4, 4, 5))
 plot(tab, pch = 16, cex = 0.6, xlab = expression(log[2]~fold~change), ylab = expression(-log[10]~pvalue))
 #signGenes = (abs(tab$logFC) > lfc & tab$negLogPval > -log10(pval))
--- workflow.sh
+++ workflow.sh
@@ -1,48 +1,70 @@
 #! /usr/bin/env bash
 
+
+# Abort if any part fails
+set -e
 cd `dirname $0`
 
 chmod +x ./R/1_getData.R
 chmod +x ./R/2_subsettingData.R
 chmod +x ./R/3_DEA.R
+chmod +x ./R/4_enrichmentAnalysis.R
+chmod +x ./R/5_keggpathway.R
 
 # Defaults
-pval=0.01
-lfc=2
-all=false
+pval=0.0001
+lfc=4
+ea=false
+skipgettingdata=false
 
 usage()
 {
     cat <<EOF
-Usage: $0 [-a] [-p pvalue=$pval] [-l logfoldchange=$lfc] [-g pdfoutputfile=DATETIME-USER-CANCERTYPE1-vs-CANCERTYPE2.pdf] [-c csvoutputfile=DATETIME-USER-CANCERTYPE1-vs-CANCERTYPE2.csv] [CANCERTYPE1 CANCERTYPE2]
+Usage: $0 [-s] [-p pvalue=$pval] [-l logfoldchange=$lfc] [-d deafilenames=DATETIME-USER-dea-CANCERTYPE1-vs-CANCERTYPE2] [-E] [-e enrichfilenames=DATETIME-USER-ea-CANCERTYPE1-vs-CANCERTYPE2] [-k pathwayids] CANCERTYPE1 CANCERTYPE2
 
-    The default values are showed after the equals charachters in usage string.
+    The default values are showed after the equals characters in usage string.
 
     -h                  Show this help message.
-    -a                  Perform both combinations of DEA. You may set pvalue and log fold change
-                        but no output file names neither cancer types as arguments.
     -p                  p-value threshold.
     -l                  Log fold change threshold.
-    -g                  PDF file output path.
-    -c                  CSV file output path.
+    -d                  Output filename for the DEA resulting files. '.pdf' and
+                        '.csv' extensions will be appended.
+    -E                  Perform enrichment analysis with default output 
+                        filename for the resulting files.
+    -e                  Perform enrichment analysis setting the output filename
+                        to resulting files. '.pdf' and '.csv' extensions will be 
+                        appended.
+    -k                  Fetch the pathway visualization from KEGG database.
+                        Provide the KEGG pathways in a comma separated string 
+                        like "-k hsa05165,hsa04915". An enrichment analysis 
+                        must be performed before this step. If no configuration
+                        is provided for previous steps they will be executed
+                        with default values. PNG images and XML data will be
+                        placed in '`dirname $0`' path.
+    -s                  Skip the getting data step, using cached data.
     CANCERTYPE          (TNBC|Basal|LumA)
 EOF
 }
 
-updateoutfilename()
+printinfo()
 {
-    outfilename="`date +%Y%m%d-%H%M%S`-$USER-$cancertype1-vs-$cancertype2"
+    echo -e "\e[36m\e[1m$1\e[0m"
+}
+
+printerror()
+{
+    echo -e "\e[31m\e[1m$1\e[0m"
 }
 
 # Parse options
-while getopts ":p:l:g:c:ah" opt; do
+while getopts ":p:l:g:c:hEe:k:s" opt; do
     case $opt in
         h)
             usage
             exit 0
             ;;
-        a)
-            all=true
+        s)
+            skipgettingdata=true
             ;;
         p)
             pval=$OPTARG
@@ -50,74 +72,63 @@ while getopts ":p:l:g:c:ah" opt; do
         l)
             lfc=$OPTARG
             ;;
-        g)
-            if [[ $all = true ]]; then
-                echo "Not used arguments: do not provide cancer types neither output file names if 'all' option (-a) is setted." >&2
-                exit 1
-            fi
-            pdfoutputfile=$OPTARG
+        d)
+            deafilenames=$OPTARG
             ;;
-        c)
-            if [[ $all = true ]]; then
-                echo "Not used arguments: do not provide cancer types neither output file names if 'all' option (-a) is setted." >&2
-                exit 1
-            fi
-            csvoutputfile=$OPTARG
+        E)
+            ea=true
+            ;;
+        e)
+            ea=true
+            eafilenames=$OPTARG
+            ;;
+        k)
+            ea=true
+            pwids=$OPTARG
             ;;
         \?)
-            echo "Invalid option: -$OPTARG" >&2
+            printerror "Invalid option: -$OPTARG."
             exit 1
             ;;
         :)
-            echo "Option -$OPTARG requires an argument." >&2
+            printerror "Option -$OPTARG requires an argument."
             exit 1
             ;;
     esac
 done
 shift $(($OPTIND - 1))
 
+
 # Check that the call is coherent
-if [[ $all = false && $# -ne 2 ]]; then
-    echo "You must provide the two cancer types for the DEA: TNBC|Basal|LumA."
-    exit 1
-fi
-if [[ $all = true && $# -ne 0 ]]; then
-    echo "Not used arguments: do not provide cancer types neither output file names if 'all' option (-a) is setted." >&2
+if [[ $# -ne 2 ]]; then
+    printerror "You must provide the two cancer types for the DEA: TNBC|Basal|LumA."
     exit 1
 fi
 
-# Perform the 4 combinations of DEA if all is setted
-if [ $all = true ]; then
-    cancertype1="LumA"
-    cancertype2="TNBC"
-    updateoutfilename
-    ./workflow.sh -p $pval -l $lfc -g $outfilename.pdf -c $outfilename.csv $cancertype1 $cancertype2
-    cancertype1="LumA"
-    cancertype2="Basal"
-    updateoutfilename
-    ./workflow.sh -p $pval -l $lfc -g $outfilename.pdf -c $outfilename.csv $cancertype1 $cancertype2
-    cancertype1="TNBC"
-    cancertype2="LumA"
-    updateoutfilename
-    ./workflow.sh -p $pval -l $lfc -g $outfilename.pdf -c $outfilename.csv $cancertype1 $cancertype2
-    cancertype1="Basal"
-    cancertype2="LumA"
-    updateoutfilename
-    ./workflow.sh -p $pval -l $lfc -g $outfilename.pdf -c $outfilename.csv $cancertype1 $cancertype2
-else
+if [ $skipgettingdata = false ]; then
+    printinfo "Getting data"
     ./R/1_getData.R
     ./R/2_subsettingData.R
-    cancertype1=$1
-    cancertype2=$2
-    # Set default filename if not provided by the user
-    updateoutfilename
-    if [ "$pdfoutputfile" = "" ]; then
-        pdfoutputfile=$outfilename.pdf
-    fi
-    if [ "$csvoutputfile" = "" ]; then
-        csvoutputfile=$outfilename.csv
-    fi
-    ./R/3_DEA.R $cancertype1 $cancertype2 $pval $lfc $pdfoutputfile $csvoutputfile
 fi
 
-# rm ./R/*.RData
+cancertype1=$1
+cancertype2=$2
+
+if [ "$deafilenames" = "" ]; then
+    deafilenames="`date +%Y%m%d-%H%M%S`-$USER-dea-$cancertype1-vs-$cancertype2"
+fi
+
+printinfo "Performing differential expression analysis: $cancertype1 vs $cancertype2."
+./R/3_DEA.R $cancertype1 $cancertype2 $pval $lfc $deafilenames
+
+if [ $ea = true ]; then
+    if [ "$eafilenames" = "" ]; then
+        eafilenames="`date +%Y%m%d-%H%M%S`-$USER-ea-$cancertype1-vs-$cancertype2"
+    fi
+    printinfo "Performing the enrichment analysis."
+    ./R/4_enrichmentAnalysis.R $pval $lfc $eafilenames
+    if [ "$pwids" != "" ]; then
+        printinfo "Fetching pathway visualization images from KEGG."
+        ./R/5_keggpathway.R $pwids
+    fi
+fi
